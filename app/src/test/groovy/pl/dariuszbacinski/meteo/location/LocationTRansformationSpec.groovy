954d529d75caf3a6472fa067da819a8af7cd7bd5
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-10-07 21:41:42 +0200

    Added test for extraction of whole list

--- app/src/test/groovy/pl/dariuszbacinski/meteo/location/LocationTRansformationSpec.groovy
+++ app/src/test/groovy/pl/dariuszbacinski/meteo/location/LocationTRansformationSpec.groovy
@@ -5,26 +5,33 @@ import org.robolectric.annotation.Config
 class LocationTransformationSpec extends ShadowRoboSpecification {
 
     public static final int SECOND_ELEMENT = 1
-    FavoriteLocationRepository favoriteLocationRepository = Mock()
 
     def "returns number of favorite locations"() {
         given:
-            favoriteLocationRepository.findAll() >> [new FavoriteLocation(), new FavoriteLocation()]
-            LocationTransformation objectUnderTest = new LocationTransformation(favoriteLocationRepository.findAll());
+            LocationTransformation objectUnderTest = new LocationTransformation([new FavoriteLocation(), new FavoriteLocation()]);
         when:
             def count = objectUnderTest.favoriteLocationCount
         then:
             count == 2
     }
 
-    def "returns correct favorite location"() {
+    def "extracts requested favorite location"() {
         given:
             def expectedLocation = new Location("", 0, 0)
-            favoriteLocationRepository.findAll() >> [new FavoriteLocation(), new FavoriteLocation(expectedLocation)]
-            LocationTransformation objectUnderTest = new LocationTransformation(favoriteLocationRepository.findAll());
+            LocationTransformation objectUnderTest = new LocationTransformation([new FavoriteLocation(), new FavoriteLocation(expectedLocation)]);
         when:
             def favoriteLocation = objectUnderTest.extractLocationAtPosition(SECOND_ELEMENT)
         then:
             favoriteLocation == expectedLocation
+    }
+
+    def "extracts all favorite locations"() {
+        given:
+            def expectedLocation = new Location("", 0, 0)
+            LocationTransformation objectUnderTest = new LocationTransformation([new FavoriteLocation(expectedLocation), new FavoriteLocation(expectedLocation)]);
+        when:
+            def favoriteLocations = objectUnderTest.extractLocations()
+        then:
+            favoriteLocations == [expectedLocation, expectedLocation]
     }
 }
