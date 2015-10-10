65ed76e3cb893b35b7bfad355d6dbeb73f8e2afb
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-10-10 15:36:00 +0200

    Fixed SwappingHolder shadow, constructor is required

--- app/src/test/groovy/pl/dariuszbacinski/meteo/shadow/ShadowSwappingHolder.groovy
+++ app/src/test/groovy/pl/dariuszbacinski/meteo/shadow/ShadowSwappingHolder.groovy
@@ -1,5 +1,7 @@
-package pl.dariuszbacinski.meteo.shadow;
+package pl.dariuszbacinski.meteo.shadow
 
+import android.view.View
+import com.bignerdranch.android.multiselector.MultiSelector;
 import com.bignerdranch.android.multiselector.SwappingHolder;
 
 import org.robolectric.annotation.Implements;
@@ -7,4 +9,7 @@ import org.robolectric.annotation.Implements;
 @Implements(SwappingHolder)
 public class ShadowSwappingHolder {
 
+    public void __constructor__(View itemView, MultiSelector multiSelector) {
+        //do not delete, required by Robolectric
+    }
 }
