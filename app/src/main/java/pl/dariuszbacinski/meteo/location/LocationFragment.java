4cfd8e24d7fa5849d7fe264bd76a578eabdd23d0
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-10-27 23:28:27 +0100

    Migrated Location Fragment to data binding

--- app/src/main/java/pl/dariuszbacinski/meteo/location/LocationFragment.java
+++ app/src/main/java/pl/dariuszbacinski/meteo/location/LocationFragment.java
@@ -4,19 +4,17 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import com.bignerdranch.android.multiselector.MultiSelector;
 import com.jakewharton.rxbinding.widget.RxTextView;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import pl.dariuszbacinski.meteo.R;
+import pl.dariuszbacinski.meteo.databinding.FragmentLocationBinding;
 import pl.dariuszbacinski.meteo.diagram.DiagramActivity;
+import rx.Subscription;
 import rx.functions.Action1;
 
 public class LocationFragment extends Fragment {
@@ -27,30 +25,25 @@ public class LocationFragment extends Fragment {
     private LocationAdapter locationAdapter;
     private FavoriteLocationRepository favoriteLocationRepository = new FavoriteLocationRepository();
     private LocationRepository locationRepository = new LocationRepository();
-
-    public LocationFragment() {
-
-    }
+    private Subscription watcherSubscription;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_location, container, false);
-        ButterKnife.bind(this, view);
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.favorites_list);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        FragmentLocationBinding locationBinding = FragmentLocationBinding.inflate(inflater, container, false);
+        ButterKnife.bind(this, locationBinding.getRoot());
         locationAdapter = new LocationAdapter(multiSelector, locationRepository.findAll(), favoriteLocationRepository.findAll());
-        recyclerView.setAdapter(locationAdapter);
-        //TODO migrate to data binding
-        EditText filterInput = (EditText) view.findViewById(R.id.favorites_filter);
-        RxTextView.textChanges(filterInput).subscribe(new Action1<CharSequence>() {
+        locationBinding.favoritesList.setHasFixedSize(true);
+        locationBinding.favoritesList.setAdapter(locationAdapter);
+        locationBinding.favoritesList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        watcherSubscription = RxTextView.textChanges(locationBinding.favoritesFilter).subscribe(new Action1<CharSequence>() {
             @Override
             public void call(CharSequence charSequence) {
                 locationAdapter.filterLocationsByName(charSequence.toString());
             }
         });
-        return view;
+        locationBinding.setFragment(this);
+        return locationBinding.getRoot();
     }
 
     @Override
@@ -67,11 +60,16 @@ public class LocationFragment extends Fragment {
         }
     }
 
-    //TODO migrate to data binding
-    @OnClick(R.id.favorites_save)
-    public void saveFavorites() {
+    public void saveFavorites(View view) {
         //TODO model should be responsible for saving favorites
         favoriteLocationRepository.saveList(locationAdapter.getFavoritePositions());
         startActivity(new Intent(getActivity(), DiagramActivity.class).setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        watcherSubscription.unsubscribe();
     }
 }
