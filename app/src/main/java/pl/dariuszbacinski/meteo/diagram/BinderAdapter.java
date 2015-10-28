b12fd9e6ae2a7fbab8c54a460ed10e805176d957
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-10-28 22:09:11 +0100

    Improved memory usage by diagram  bitmaps

--- app/src/main/java/pl/dariuszbacinski/meteo/diagram/BinderAdapter.java
+++ app/src/main/java/pl/dariuszbacinski/meteo/diagram/BinderAdapter.java
@@ -1,6 +1,7 @@
 package pl.dariuszbacinski.meteo.diagram;
 
 import android.databinding.BindingAdapter;
+import android.graphics.Bitmap;
 
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
@@ -12,7 +13,7 @@ public class BinderAdapter {
     @BindingAdapter("bind:imageUrl")
     public static void loadImage(PhotoView imageView, String url) {
         imageView.setScaleLevels(1f, 1.5f, 2f);
-        Picasso.with(imageView.getContext()).load(url).memoryPolicy(MemoryPolicy.NO_STORE).fit().centerInside().noFade().into(imageView);
+        Picasso.with(imageView.getContext()).load(url).config(Bitmap.Config.RGB_565).memoryPolicy(MemoryPolicy.NO_STORE).noFade().into(imageView);
     }
 }
 
