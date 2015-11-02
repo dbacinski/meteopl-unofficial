9b9126ca17e531c937b12bdf7b6719070af04982
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-11-02 12:32:33 +0100

    Extracted scale sizes to resources

--- app/src/main/java/pl/dariuszbacinski/meteo/diagram/BinderAdapter.java
+++ app/src/main/java/pl/dariuszbacinski/meteo/diagram/BinderAdapter.java
@@ -6,13 +6,17 @@ import android.graphics.Bitmap;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
 
+import pl.dariuszbacinski.meteo.R;
 import uk.co.senab.photoview.PhotoView;
 
 public class BinderAdapter {
 
     @BindingAdapter("bind:imageUrl")
     public static void loadImage(PhotoView imageView, String url) {
-        imageView.setScaleLevels(1f, 1.5f, 2f);
+        float minScale = imageView.getContext().getResources().getInteger(R.integer.image_scale_min);
+        float maxScale = imageView.getContext().getResources().getInteger(R.integer.image_scale_max);
+        float mediumScale = (maxScale + minScale) / 2f;
+        imageView.setScaleLevels(minScale, mediumScale, maxScale);
         Picasso.with(imageView.getContext()).load(url).config(Bitmap.Config.RGB_565).memoryPolicy(MemoryPolicy.NO_STORE).noFade().into(imageView);
     }
 }
