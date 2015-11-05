a1a73c5574ba9cbd5565b9eb7b1f5e5b2d46c504
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-11-05 18:58:36 +0100

    Removed test crash

--- app/src/main/java/pl/dariuszbacinski/meteo/location/LocationActivity.java
+++ app/src/main/java/pl/dariuszbacinski/meteo/location/LocationActivity.java
@@ -11,6 +11,5 @@ public class LocationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-        throw new RuntimeException();
     }
 }
