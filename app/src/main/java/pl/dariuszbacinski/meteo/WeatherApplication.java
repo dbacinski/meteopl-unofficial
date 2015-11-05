5eed72b482d86ced552942b2dbcd2204e0290e84
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-11-05 18:00:45 +0100

    Added Crashlitics

--- app/src/main/java/pl/dariuszbacinski/meteo/WeatherApplication.java
+++ app/src/main/java/pl/dariuszbacinski/meteo/WeatherApplication.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
 import com.activeandroid.ActiveAndroid;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+
+import io.fabric.sdk.android.Fabric;
 
 public class WeatherApplication extends MultiDexApplication {
 
@@ -16,6 +20,9 @@ public class WeatherApplication extends MultiDexApplication {
         super.onCreate();
         ActiveAndroid.initialize(this);
         refWatcher = LeakCanary.install(this);
+
+        Fabric.with(this, new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build());
     }
 
     public static RefWatcher getRefWatcher(Context context) {
--- app/src/main/java/pl/dariuszbacinski/meteo/location/LocationActivity.java
+++ app/src/main/java/pl/dariuszbacinski/meteo/location/LocationActivity.java
@@ -11,5 +11,6 @@ public class LocationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        throw new RuntimeException();
     }
 }
