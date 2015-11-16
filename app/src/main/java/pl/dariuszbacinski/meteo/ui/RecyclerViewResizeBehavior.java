a3e61270216b713357410e6c5a6f9888b4bd35f6
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-11-16 20:12:14 +0100

    Resizing RecycleView

---
+++ app/src/main/java/pl/dariuszbacinski/meteo/ui/RecyclerViewResizeBehavior.java
@@ -0,0 +1,33 @@
+package pl.dariuszbacinski.meteo.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+
+public class RecyclerViewResizeBehavior extends CoordinatorLayout.Behavior<RecyclerView> {
+
+    private int height;
+
+    public RecyclerViewResizeBehavior(Context context, AttributeSet attrs) {
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, RecyclerView child, View dependency) {
+        return dependency instanceof Button;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, RecyclerView child, View dependency) {
+        int translationY = Math.min(0, (int) dependency.getTranslationY());
+        child.getLayoutParams().height = getHeight((View) child.getParent()) + translationY - getHeight(dependency) - parent.getPaddingBottom();
+        child.requestLayout();
+        return true;
+    }
+
+    private int getHeight(View child) {
+        return child.getMeasuredHeight();
+    }
+}
