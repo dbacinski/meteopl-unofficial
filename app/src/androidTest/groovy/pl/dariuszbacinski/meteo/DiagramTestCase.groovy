7dc1e02239bf25efd78a159c06bcbacf5306077e
dariusz.bacinski <dariusz.bacinski@gmail.com>
2015-11-21 08:50:21 +0100

    import fix

--- app/src/androidTest/groovy/pl/dariuszbacinski/meteo/DiagramTestCase.groovy
+++ app/src/androidTest/groovy/pl/dariuszbacinski/meteo/DiagramTestCase.groovy
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith
 import pl.dariuszbacinski.meteo.location.view.LocationActivity
 
 import static android.support.test.espresso.Espresso.onView
+import static android.support.test.espresso.action.ViewActions.typeText
 import static android.support.test.espresso.assertion.ViewAssertions.matches
 import static android.support.test.espresso.matcher.ViewMatchers.withId
 import static android.support.test.espresso.matcher.ViewMatchers.withText
